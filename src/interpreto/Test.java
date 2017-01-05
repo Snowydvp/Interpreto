0c8914fbd9fd3bb4bfe2b438e375550b8c1ec41c
simon <simon@Lenovo-Simon>
2017-01-05 16:12:07 +0100

    v

--- src/interpreto/Test.java
+++ src/interpreto/Test.java
@@ -1,22 +1,11 @@
 package interpreto;
 
-import bsh.EvalError;
-import bsh.Interpreter;
 import interpreto.Metier.Type.*;
 
 public class Test {
 
 	public static void main(String args[]) {
 		Booleen b = new Booleen("bool", false);
-		
-		Interpreter interpreter = new Interpreter();
-		try {
-			interpreter.eval("Entier a = 5");
-			System.out.println(interpreter.get("a"));
-		} catch (EvalError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		new Controleur();
 	}
 }
