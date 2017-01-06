e1879edf71bb513ef1f1c93707362fd7e0d15ab9
Snowy <Snowy@172.30.6.179>
2017-01-06 15:35:39 +0100

    touche que a CUI

--- src/interpreto/Metier/AnalyseCode.java
+++ src/interpreto/Metier/AnalyseCode.java
@@ -11,10 +11,23 @@ public class AnalyseCode {
 	public AnalyseCode(String fichier) {
 		codeBrut = new LectureFichier(fichier).getCode();
 		codeAnalyse = new ArrayList<>(codeBrut);
-		colorierPrimitives();
+		//colorierPrimitives();
 	}
 
-	private void colorierPrimitives() {
+	private ArrayList<String> colorierPrimitives(ArrayList<String> mots) {
+		ArrayList<String> motsColor = new ArrayList<>();
+		for (String mot : mots) {
+			if (estMotCle(mot)) {
+				// coloriage de la primitive en bleu
+				mot = "\u001B[1;36m" + mot + "\u001B[0m";
+				traiter(ligneBrut.substring(ligneBrut.indexOf(motBrut), ligneBrut.lastIndexOf(')') + 1));
+			} else
+				motAnalyse += motBrut;
+			ligneAnalyse += motAnalyse + ' ';
+		}
+	}
+
+	private ArrayList<String> getMots() {
 		for (int cptLig = 0; cptLig < codeBrut.size(); cptLig++) {
 			String ligneAnalyse = "";
 			String ligneBrut = codeBrut.get(cptLig);
@@ -25,18 +38,13 @@ public class AnalyseCode {
 			while (scLigne.hasNext()) {
 				String motBrut = scLigne.next();
 				String motAnalyse = "";
-				if (estMotCle(motBrut))
-					// coloriage de la primitive en bleu
-					motAnalyse += "\u001B[1;36m" + motBrut + "\u001B[0m";
-				else
-					motAnalyse += motBrut;
-				ligneAnalyse += motAnalyse + ' ';
+
+				
 
 			}
 
 			codeAnalyse.set(cptLig, ligneAnalyse);
 		}
-
 	}
 
 	/**
@@ -62,10 +70,13 @@ public class AnalyseCode {
 	public ArrayList<String> getCodeAnalyse() {
 		return codeAnalyse;
 	}
-	
-	public ArrayList<String> getCodeBrut()
-	{
+
+	public ArrayList<String> getCodeBrut() {
 		return this.codeBrut;
+	}
+
+	public void traiter(String motCle) {
+		System.out.println(motCle);
 	}
 
 }
